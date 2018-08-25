d9d7aee8c8af6ba8041d129f1cd5559f72b0706c
Tony Nyagah <tony.m.nyagah@gmail.com>
2018-08-25 20:18:47 +0300

    modified the xml output to look more like a myanimelist export

--- main.py
+++ main.py
@@ -1,150 +1,200 @@
 import requests
-import pprint
+import json
 
 query = '''
-query ($username: String) {
-  MediaListCollection(userName: $username, type: ANIME) {
-    statusLists {
-      status
-      score(format: POINT_10)
-      progress
-      progressVolumes
-      repeat
-      priority
-      notes
-      startedAt { year, month, day }
-      completedAt { year, month, day }
-      media {
-        idMal
-        episodes
-        title { romaji }
-        chapters
-        volumes
-        siteUrl
-      }
-    }
+query ($username: String, $type: MediaType) {
+  MediaListCollection(userName: $username, type: $type) {
+	lists {
+	  entries {
+		status
+		score(format: POINT_10)
+		progress
+		progressVolumes
+		repeat
+		priority
+		notes
+		startedAt { year, month, day }
+		completedAt { year, month, day }
+		media {
+		  idMal
+		  title { romaji }
+		  episodes
+		  chapters
+		  volumes
+		  siteUrl
+		}
+	  }
+	}
   }
 }
 '''
 
-# Define our query variables and values that will be used in the query request
 variables = {
-  'username': 'ynot254'
+	'username': 'ynot254',
+	'type': 'ANIME'
 }
 
 url = 'https://graphql.anilist.co'
 
 def main():
-#   print('''
-#                   ┏━━━━━━━━━━━━━━━━━━┓
-#                   ┃  AniList to MAL  ┃
-#                   ┗━━━━━━━━━━━━━━━━━━┛
-
-# An export tool for Anilist to import to MyAnimeList.
-# Enter your username, and this will generate a MyAnimeList
-# compatible XML file to import at https://myanimelist.net/import.php
-
-# Made by Nathan Wentworth (https://nathanwentworth.co)
-
-# Report any problems to me on twitter! https://twitter.com/nathanwentworth
-# ''')
-
-  print('''
-  ┏━━ AniList to MAL ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-  ┃ An export tool for Anilist to import to MyAnimeList.          ┃
-  ┃ Enter your username, and this will generate an XML file       ┃
-  ┃ to import here: https://myanimelist.net/import.php            ┃
-  ┃ Made by Nathan Wentworth (https://nathanwentworth.co)         ┃
-  ┃ Report any problems here: https://twitter.com/nathanwentworth ┃
-  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+	print('''
+	┏━━ AniList to MAL ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+	┃ An export tool for Anilist to import to MyAnimeList.          ┃
+	┃ Enter your username, and this will generate an XML file       ┃
+	┃ to import here: https://myanimelist.net/import.php            ┃
+	┃ Made by Nathan Wentworth (https://nathanwentworth.co)         ┃
+	┃ Modified by Antony Nyagah (https://tonynyagah.github.io)      ┃
+	┃ Report any problems here: https://twitter.com/NyagahTony      ┃
+	┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 ''')
-
-  getUserData()
+	getUserData()
 
 def getUserData():
-  variables['username'] = input("→ Anilist username: ")
+	variables['username'] = input("→ Anilist username: ")
 
-  if variables['username'] == '':
-    print('Please enter a valid username!')
-    getUserData()
-  else:
-    getAnilistData()
+	if variables['username'] == '':
+		print('Please enter a valid username!')
+		getUserData()
+	else:
+		getAnilistData()
 
+# Make the HTTP Api request
 def getAnilistData():
-  # Make the HTTP Api request
-  response = requests.post(url, json={'query': query, 'variables': variables})
-  jsonData = response.json()
-  statusLists = jsonData['data']['MediaListCollection']['statusLists']
-  printAniListData(statusLists)
-  convertAnilistDataToXML(statusLists)
+	response = requests.post(url, json={'query': query, 'variables': variables})
+	jsonData = response.json()
+	listEntries = jsonData['data']['MediaListCollection']['lists']
+	# prettified results for easy reading
+	readableData = json.dumps(listEntries, indent=2, sort_keys=True) 
+	# print(readableData)
+	printAnilistData(listEntries)
+	convertAnilistData(listEntries)
 
-def printAniListData(data):
-  print('↓ Exported List ↓')
-  for listStatus in data:
-    print('\n##### ' + listStatus.capitalize() + ' #####')
-    for item in data[listStatus]:
-      print(' - ' + str(item['media']['title']['romaji']))
-  print('\n✔︎ Successfully exported!')
-  print('\nGo to https://myanimelist.net/import.php and select "MyAnimeList Import" under import type.\n')
+def printAnilistData(data):
 
-def convertAnilistDataToXML(data):
-  output = '''<?xml version="1.0" encoding="UTF-8" ?>
-    <!--
-     Created by XML Export feature at MyAnimeList.net
-     Programmed by Xinil
-     Last updated 5/27/2008
-    -->
+	listLength = len(data); # 5 because from current to planning
 
-    <myanimelist>
+	print('↓ Exported List ↓')
+	for x in range(0, listLength):
+		print('\n##### ' + data[x]['entries'][x]['status'] + ' #####')
+		entryLength = len(data[x]['entries'])
+		for e in range(0, entryLength):
+			print(' - ' + data[x]['entries'][e]['media']['title']['romaji'])
+	print('\n✔︎ Successfully exported!')
+	print('\nGo to https://myanimelist.net/import.php and select "MyAnimeList Import" under import type.\n')
 
-      <myinfo>
-        <user_id>526891</user_id>
-        <user_name>nathanwentworth</user_name>
-        <user_export_type>1</user_export_type>
-        <user_total_anime>357</user_total_anime>
-        <user_total_watching>6</user_total_watching>
-        <user_total_completed>150</user_total_completed>
-        <user_total_onhold>14</user_total_onhold>
-        <user_total_dropped>27</user_total_dropped>
-        <user_total_plantowatch>160</user_total_plantowatch>
-      </myinfo>
+def convertAnilistData(data):
 
-'''
+	plan_to_watch = len(data[0]['entries'])
+	completed = len(data[1]['entries'])
+	watching = len(data[2]['entries'])
+	dropped = len(data[3]['entries'])
+	on_hold = len(data[4]['entries'])
+	total = plan_to_watch + completed + dropped + on_hold + watching
+	
+	startLine = '''<?xml version="1.0" encoding="UTF-8" ?>
+		<!--
+		 Created by XML Export feature at MyAnimeList.net
+		 Programmed by Xinil
+	     Last updated 5/27/2008
+		-->
 
-  for listStatus in data:
-    for item in data[listStatus]:
-      s = str(item['status'])
-      if s == "PLANNING":
-        s = "Plan to Watch"
-      elif s == "DROPPED":
-        s = "Dropped"
-      elif s == "CURRENT":
-        s = "Watching"
-      elif s == "PAUSED":
-        s = "On-Hold"
+		<myanimelist>
 
-      animeItem = ''
-      animeItem += '        <anime>\n'
-      animeItem += '          <series_animedb_id>' + str(item['media']['idMal']) + '</series_animedb_id>\n'
-      animeItem += '          <series_episodes>' + str(item['media']['episodes']) + '</series_episodes>\n'
-      animeItem += '          <my_watched_episodes>' + str(item['progress']) + '</my_watched_episodes>\n'
-      animeItem += '          <my_score>' + str(item['score']) + '</my_score>\n'
-      animeItem += '          <my_status>' + s + '</my_status>\n'
-      animeItem += '          <my_times_watched>' + str(item['repeat']) + '</my_times_watched>\n'
+	'''
 
-      animeItem += '          <update_on_import>1</update_on_import>\n'
-      animeItem += '        </anime>\n\n'
+	userInfo = '        <myinfo>\n'
+	userInfo += '                <user_id>4381285</user_id>\n'
+	userInfo += '                <user_name>' + variables['username'] + '</user_name>\n'
+	userInfo += '                <user_export_type>1</user_export_type>\n'
+	userInfo += '                <user_total_anime>' + str(total) + '</user_total_anime>\n'
+	userInfo += '                <user_total_watching>' + str(watching) + '</user_total_watching>\n'
+	userInfo += '                <user_total_completed>' + str(completed) + '</user_total_completed>\n'
+	userInfo += '                <user_total_onhold>'+ str(on_hold) +'</user_total_onhold>\n'
+	userInfo += '                <user_total_dropped>' + str(dropped) + '</user_total_dropped>\n'
+	userInfo += '                <user_total_plantowatch>' +str(plan_to_watch) + '</user_total_plantowatch>\n'
+	userInfo += '            </myinfo>\n\n\n'
 
-      output += animeItem
+	mediaInfo = ''
 
-  output += '      </myanimelist>'
+	finishLine = '            </myanimelist>'
+	
+	for n in range(0, len(data)):
+		for i in range(0, len(data[n]['entries'])):
 
-  writeToXMLFile(output)
+            # the results of the dates come back as none
+            # if the show has no started or completed date
+            # and get printed as 'None' to the xml file
+            # this changes them to zeros as required
+			yearStarted = data[n]['entries'][i]['startedAt']['year']
+			monthStarted = data[n]['entries'][i]['startedAt']['month']
+			dayStarted = data[n]['entries'][i]['startedAt']['day']
+			yearCompleted = data[n]['entries'][i]['completedAt']['year']
+			monthCompleted = data[n]['entries'][i]['completedAt']['month']
+			dayCompleted = data[n]['entries'][i]['completedAt']['day']
+
+			if yearStarted == None:
+				yearStarted = '0000'
+			if yearCompleted == None:
+				yearCompleted = '0000'
+			if monthStarted == None:
+				monthStarted = '00'
+			if monthCompleted == None:
+				monthCompleted = '00'
+			if dayStarted == None:
+				dayStarted = '00'
+			if dayCompleted == None:
+				dayCompleted = '00'
+
+            # anilist and mal name the status of each anime/manga
+            # differently so this takes the status from anilist
+            # and changes it to one readable by myanimelist
+			status = data[n]['entries'][i]['status']
+
+			if status == "PLANNING":
+				status = "Plan to Watch"
+			elif status == "DROPPED":
+				status = "Dropped"
+			elif status == "CURRENT":
+				status = "Watching"
+			elif status == "PAUSED":
+				status = "On-Hold"
+			elif status == "COMPLETED":
+				status = "Completed"
+
+
+			mediaInfo += '				<anime>\n'
+			mediaInfo += '					<series_animedb_id>' + str(data[n]['entries'][i]['media']['idMal']) + '</series_animedb_id>\n'
+			mediaInfo += '                    <series_title><![CDATA[' + str(data[n]['entries'][i]['media']['title']['romaji']) + ']]></series_title>\n'
+			mediaInfo += '					<series_type></series_type>\n'
+			mediaInfo += '                    <series_episodes>' + str(data[n]['entries'][i]['media']['episodes']) +'</series_episodes>\n'
+			mediaInfo += '					<my_id>0</my_id>\n'
+			mediaInfo += '					<my_watched_episodes>' + str(data[n]['entries'][i]['progress']) + '</my_watched_episodes>\n'
+			mediaInfo += '					<my_start_date>' + str(yearStarted) + '-' + str(monthStarted) + '-' + str(dayStarted) + '</my_start_date>\n'
+			mediaInfo += '					<my_finish_date>' + str(yearCompleted) + '-' + str(monthCompleted) + '-' + str(dayCompleted) + '</my_finish_date>\n'
+			mediaInfo += '					<my_rated></my_rated>\n'
+			mediaInfo += '					<my_score>' + str(data[n]['entries'][i]['score']) + '</my_score>\n'
+			mediaInfo += '					<my_dvd></my_dvd>\n'
+			mediaInfo += '					<my_storage></my_storage>\n'
+			mediaInfo += '					<my_status>' + str(status) + '</my_status>\n'
+			mediaInfo += '					<my_comments><![CDATA[]]></my_comments>\n'
+			mediaInfo += '					<my_times_watched>' + str(data[n]['entries'][i]['repeat']) + '</my_times_watched>\n'
+			mediaInfo += '					<my_rewatch_value></my_rewatch_value>\n'
+			mediaInfo += '					<my_tags><![CDATA[]]></my_tags>\n'
+			mediaInfo += '					<my_rewatching>0</my_rewatching>\n'
+			mediaInfo += '					<my_rewatching_ep>0</my_rewatching_ep>\n'
+			mediaInfo += '					<update_on_import>1</update_on_import>\n'
+			mediaInfo += '				</anime>\n\n'
+	
+	
+	output = startLine + userInfo + mediaInfo + finishLine
+	# print(output)
+	print("Anime count: %d" % total)
+
+	writeToXMLFile(output)
 
 def writeToXMLFile(output):
-  f = open('anilist-export.xml', 'w')
-  f.write(output)
-  f.close()
-
+	f = open('my-anilist-export.xml', 'w+')
+	f.write(output)
+	f.close()
 
 main()
